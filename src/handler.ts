d9740ba2e5ce8ccfb5a628a9100372e6af84c844
kaddo <kofi.addo@facile.it>
2020-03-14 15:14:00 +0100

    feat: lambda handler

---
+++ src/handler.ts
@@ -0,0 +1,121 @@
+import {
+  ErrorHandler,
+  HandlerInput,
+  RequestHandler,
+  SkillBuilders
+} from "ask-sdk-core";
+import { Response, SessionEndedRequest } from "ask-sdk-model";
+
+const SKILL_NAME = "Meetup Events";
+
+const LaunchRequestHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return handlerInput.requestEnvelope.request.type === "LaunchRequest";
+  },
+  handle(handlerInput: HandlerInput): Response {
+    const speechText = `Benvenuto su ${SKILL_NAME}. Puoi ottenere tutti gli eventi Meetup dicendo i miei eventi`;
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard(SKILL_NAME, speechText)
+      .getResponse();
+  }
+};
+
+const MeetupIntentHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === "MeetupIntent"
+    );
+  },
+  handle(handlerInput: HandlerInput): Response {
+    const speechText = "Sono ancora in fase di sviluppo";
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard(SKILL_NAME, speechText)
+      .getResponse();
+  }
+};
+
+const HelpIntentHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === "AMAZON.HelpIntent"
+    );
+  },
+  handle(handlerInput: HandlerInput): Response {
+    const speechText =
+      "Puoi ottenere tutti gli eventi Meetup dicendo i miei eventi";
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard(SKILL_NAME, speechText)
+      .getResponse();
+  }
+};
+
+const CancelAndStopIntentHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      (handlerInput.requestEnvelope.request.intent.name ===
+        "AMAZON.CancelIntent" ||
+        handlerInput.requestEnvelope.request.intent.name ===
+          "AMAZON.StopIntent")
+    );
+  },
+  handle(handlerInput: HandlerInput): Response {
+    const speechText = "A presto!";
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard(SKILL_NAME, speechText)
+      .withShouldEndSession(true)
+      .getResponse();
+  }
+};
+
+const SessionEndedRequestHandler: RequestHandler = {
+  canHandle(handlerInput: HandlerInput): boolean {
+    return handlerInput.requestEnvelope.request.type === "SessionEndedRequest";
+  },
+  handle(handlerInput: HandlerInput): Response {
+    console.log(
+      `La sessione si è conclusa per via: ${
+        (handlerInput.requestEnvelope.request as SessionEndedRequest).reason
+      }`
+    );
+
+    return handlerInput.responseBuilder.getResponse();
+  }
+};
+
+const ErrorHandler: ErrorHandler = {
+  canHandle(handlerInput: HandlerInput, error: Error): boolean {
+    return true;
+  },
+  handle(handlerInput: HandlerInput, error: Error): Response {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak("Scusa, non riesco a capire il comando. Per favore, ripeti.")
+      .reprompt("Scusa, non riesco a capire il comando. Per favore, ripeti.")
+      .getResponse();
+  }
+};
+
+exports.meetupHandler = SkillBuilders.custom()
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    MeetupIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
